aa04f1582d13d694d08dc5becfd709275cfc14d6
张文军 <zhangwenjun@xiaobu121.com>
2020-04-30 18:52:49 +0800

    移除对tools的依赖

--- Example/Pods/Target Support Files/Pods-FFAPIs_Example/Pods-FFAPIs_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-FFAPIs_Example/Pods-FFAPIs_Example-frameworks.sh
@@ -163,16 +163,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FFAPIs/FFAPIs.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FFToolsKit/FFToolsKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FFAPIs/FFAPIs.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FFToolsKit/FFToolsKit.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
